08e4369fe0bffc8e93b2e3287c7a5e54ad430b6b
Pim Hakkert <43008188+pimhakkert@users.noreply.github.com>
2021-01-17 20:03:03 +0100

    Excepties worden in de productie omgeving naar de ontwikkelaars verstuurd per e-mail

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\ErrorHandler\ErrorRenderer\HtmlErrorRenderer;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,5 +40,48 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Throwable  $exception
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function report(Throwable $exception)
+    {
+        if ($this->shouldReport($exception)) {
+            $this->sendEmail($exception); // sends an email
+        }
+    }
+
+    /**
+     * Sends an email to the developer about the exception.
+     *
+     * @param Throwable $exception
+     * @return void
+     */
+    private function sendEmail(Throwable $exception)
+    {
+        if(\env('APP_ENV') !== 'local')
+        {
+            try {
+                $e = FlattenException::createFromThrowable($exception);
+                $handler = new HtmlErrorRenderer(true); // boolean, true raises debug flag...
+                $css = $handler->getStylesheet();
+                $content = $handler->getBody($e);
+
+                Mail::send('emails.exception', compact('css','content'), function ($message) {
+                    $message
+                        ->to(['auto@hakkertmedia.nl','brianloman@hotmail.com']) //1 = e-mail van Pim. Hier komen de e-mails altijd aan
+                        ->subject('KSEC website exception: ' . \Request::fullUrl())
+                    ;
+                });
+            } catch (Throwable $ex) {
+                Log::error($ex);
+            }
+        }
     }
 }
