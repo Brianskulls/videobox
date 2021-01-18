cb8072d7887367899c6698d17fe4340b9ae18e90
Pim Hakkert <43008188+pimhakkert@users.noreply.github.com>
2021-01-18 22:25:59 +0100

    Permissions hotfix

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -11,7 +11,7 @@ class PermissionRoleTableSeeder extends Seeder
     public function run()
     {
         // Admin has Users and Tasks tab
-        Role::findOrFail(1)->permissions()->attach([1, 2, 3, 4, 5]);
+        Role::findOrFail(1)->permissions()->attach([1, 2, 3, 4, 5, 6]);
         // User has Tasks tab
         Role::findOrFail(2)->permissions()->attach([2, 5]);
         // Reporter has Tasks and Video tab
